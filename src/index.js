d21e51b31d27123aea407b0a48249af1534cbcd6
Minchan Jung <minchanjung1@gmail.com>
2021-12-02 15:41:54 +0000

    created basic skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("hello");
+//module to work with doms
+const dom = (() => {
+    const addElement = (element, parentElement) => {
+        parentElement.appendChild(element);
+    };
+    const removeElement = (element) => {
+        element.remove()
+    }
+    const getElement = (element) => {
+        return element;
+    }
+})();
+
+//creating new projects factory function
+const projectFactory = (name) => {
+    const getName = () => name;
+    return {getName};
+}
+
+const taskFactory = (name, description, time) => {
+    const getName = () => name;
+    const getDescription = () => description;
+    const getTime = () => time;
+
+    return {getName, getDescription, getTime};
+}
