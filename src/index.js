d368bceed3e4c2aa858562c11ae6091694278690
Minchan Jung <minchanjung1@gmail.com>
2021-12-04 14:42:52 +0000

    mock styling and button functionality done

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const projectFactory = (name) => {
     return {getName};
 }
 
+//factory to create the actual tabs
 const taskFactory = (name, description, time) => {
     const getName = () => name;
     const getDescription = () => description;
@@ -24,3 +25,51 @@ const taskFactory = (name, description, time) => {
 
     return {getName, getDescription, getTime};
 }
+
+//module to open and close the project form
+const projectPopup = (() => {
+    const addProjectPopup = document.querySelector("#addProjectPopup");
+    const newProjectBtn = document.querySelector("#newProject");
+    const cancelBtn = document.querySelector("#projectCancel");
+    const submitBtn = document.querySelector("#projectSubmit");
+    const input = document.querySelector("#projectInput");
+
+    newProjectBtn.onclick = function() {
+        newProjectBtn.style.display = "none";
+        addProjectPopup.style.display = "block";
+    }
+
+    cancelBtn.onclick = function() {
+        newProjectBtn.style.display = "block";
+        addProjectPopup.style.display = "none";
+        input.value = "";
+    }
+
+    submitBtn.onclick = function() {
+
+    }
+})();
+
+//module to open and close task form
+const taskPopup = (() => {
+    const newTaskBtn = document.querySelector("#newTask");
+    const addTaskPopup = document.querySelector("#addTaskPopup");
+    const cancelBtn = document.querySelector("#taskCancel");
+    const submitBtn = document.querySelector("#taskSubmit");
+    const input = document.querySelector("#taskInput");
+
+    newTaskBtn.onclick = function() {
+        newTaskBtn.style.display = "none";
+        addTaskPopup.style.display = "block";
+    }
+
+    cancelBtn.onclick = function() {
+        newTaskBtn.style.display = "block";
+        addTaskPopup.style.display = "none";
+        input.value = "";
+    }
+
+    submitBtn.onclick = function() {
+
+    }
+})();
